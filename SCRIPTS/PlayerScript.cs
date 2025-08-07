503dd7b1fd66858e1edbe30afc451ddea4070926
Marcus <marcusvnp2112@gmail.com>
2025-08-07 01:45:30 -0400

    defines player node and physics

--- SCRIPTS/PlayerScript.cs
+++ SCRIPTS/PlayerScript.cs
@@ -3,4 +3,16 @@ using System;
 
 public partial class PlayerScript : Node2D
 {
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("mouse_left_click")) // Quando clicar com o botão esquerdo, pega as coordenadas do mouse
+        {
+            GD.Print(GetViewport().GetMousePosition());
+        }
+    }
 }
